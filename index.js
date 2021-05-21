b496cad3b7a50db6020b5d0b6edbcb510fb5e0d0
“HanappiZ” <Zenon.Hanappi@gmail.com>
2021-05-21 11:05:12 +0200

    next steps

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const ws = require('ws')
 const cors = require('cors')
+const SocketIO = require('socket.io')
+
 
 const corsOptions = {
     origin: 'http://localhost:8080'
@@ -11,13 +12,6 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const wsServer = new ws.Server({ noServer: true });
-
-app.use((req, res, next) => {
-    res.wss = wsServer
-    next()
-})
-
 app.get('/', (req, res) => {
     res.json({
         message: 'Welcome to Rock-Paper-Scissors API'
@@ -25,19 +19,26 @@ app.get('/', (req, res) => {
 })
 
 const http = require('http').createServer(app)
+const io = SocketIO(http, {
+    cors: {}
+})
+const messages = []
+io.on('connection', (socket) => {
+    console.log('got a connection')
+
+    socket.on('message', (arg) => {
+        const message = `${socket.id} is on screen ${arg.screen}`
+        messages.push(message)
+        io.emit('serverMessage', message)
+    
+    })
+    socket.emit('serverMessages', messages)
+})
 
 const PORT = process.env.PORT || 8081
 
-wsServer.on('connection', socket => {
-    socket.on('message', message => console.log(message));
-})
 
 const server = http.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`)
 })
 
-server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, socket => {
-        wsServer.emit('connection', socket, request)
-    })
-})
